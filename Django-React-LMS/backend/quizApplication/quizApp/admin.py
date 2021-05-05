eeb27173926d786fdce68d541283f8158dc2bed1
NaanPradeep <58743127+NaanPradeep@users.noreply.github.com>
2021-05-05 15:07:29 +0530

    Delete Django-React-LMS/backend/quizApplication/quizApp directory

--- Django-React-LMS/backend/quizApplication/quizApp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Assesment, Question, AssesmentPerformance, Choices 
-
-admin.site.register(Assesment)
-admin.site.register(AssesmentPerformance)
-admin.site.register(Question)
-admin.site.register(Choices)
--- Django-React-LMS/backend/quizApplication/quizApp/assess_perform_urls/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from quizApp.views import StudentPerformanceCreateView, StudentPerformanceListView
-
-urlpatterns = [
-    path('', StudentPerformanceListView.as_view()),
-    path('create-new/', StudentPerformanceCreateView.as_view()),
-]
